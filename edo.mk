LineageOS/android_device_sony_sm8250-common

sm8250-common: Build lineage health HAL and related sepolicies
Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

edo.mk
@@ -237,6 +237,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
+    ueventd.sony.rc \
     fstab.qcom \
     texfat.rc \
     init.nfc.rc \
@@ -334,6 +335,10 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.sony
 
 # Media
 PRODUCT_COPY_FILES += \

hellobbn/android_device_sony_edo-common

edo.mk
@@ -237,6 +237,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
+    ueventd.sony.rc \
     fstab.qcom \
     texfat.rc \
     init.nfc.rc \
@@ -334,6 +335,10 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
 
 # Media
 PRODUCT_COPY_FILES += \